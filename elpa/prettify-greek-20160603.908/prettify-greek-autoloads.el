66a89ddf7b0213513f119f8717a9125817ac5c85
dr-neptune <mrose4@worcester.edu>
2019-12-09 23:24:52 -0500

    initial commit

---
+++ elpa/prettify-greek-20160603.908/prettify-greek-autoloads.el
@@ -0,0 +1,15 @@
+;;; prettify-greek-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; prettify-greek-autoloads.el ends here
---
+++ elpa/prettify-greek-20160603.908/prettify-greek-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "prettify-greek" "20160603.908" "Greek letters for prettify-symbols" 'nil :commit "698d07a6ffe85f6fb53f3bfec4f49380c25cfd90" :keywords '("faces") :url "https://gitlab.com/fommil/emacs-prettify-greek")
